fa4bc41179be2a5293578e40e974505f5c1dfa84
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-14 15:00:58 -0300

    feat: implementando logout na aplicacao

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +10,22 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router){
+  constructor(
+    private router: Router, 
+    private service: AuthService,
+    private toast: ToastrService
+    ){
 
   }
 
   ngOnInit(): void {
     this.router.navigate(['home'])
+  }
+
+  logout(){
+    this.router.navigate(['login']);
+    this.service.logout();
+    this.toast.info("Logout realizado com sucesso!", "Logout", {timeOut: 7000});
   }
 
 }
