35884ea3dcad7137d2c3c2820bfc3aae9bfb1431
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-15 19:49:43 -0300

    feat: implementando tela de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthGuard } from './auth/auth.guard';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NavComponent } from "./components/nav/nav.component";
+import { HomeComponent } from "./components/home/home.component";
+import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AuthGuard } from "./auth/auth.guard";
+import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
 
 const routes: Routes = [
   {
-    path: 'login', component: LoginComponent
+    path: "login",
+    component: LoginComponent,
   },
   {
-    path: '',component: NavComponent, canActivate: [AuthGuard], children: [
-      {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
-    ]
-  }
+    path: "",
+    component: NavComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: "home", component: HomeComponent },
+
+      { path: "tecnicos", component: TecnicoListComponent },
+      { path: "tecnicos/create", component: TecnicoCreateComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,31 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
-
   constructor(
-    private router: Router, 
+    private router: Router,
     private service: AuthService,
     private toast: ToastrService
-    ){
-
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(["tecnicos/create"]);
   }
 
-  logout(){
-    this.router.navigate(['login']);
+  logout() {
+    this.router.navigate(["login"]);
     this.service.logout();
-    this.toast.info("Logout realizado com sucesso!", "Logout", {timeOut: 7000});
+    this.toast.info("Logout realizado com sucesso!", "Logout", {
+      timeOut: 7000,
+    });
   }
-
 }
