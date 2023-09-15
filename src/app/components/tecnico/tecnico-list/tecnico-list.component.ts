ac31c4b43b23699d7504fd0667d5fff71495fd04
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-15 16:31:30 -0300

    feat: finalizando o componente TecnicoList

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,39 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { Tecnico } from 'src/app/models/tecnico';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
-  selector: 'app-tecnico-list',
-  templateUrl: './tecnico-list.component.html',
-  styleUrls: ['./tecnico-list.component.css']
+  selector: "app-tecnico-list",
+  templateUrl: "./tecnico-list.component.html",
+  styleUrls: ["./tecnico-list.component.css"],
 })
 export class TecnicoListComponent implements OnInit {
+  TECNICO_DATA: Tecnico[] = [];
 
-  TECNICO_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Igor',
-      cpf: '123.456.789-10',
-      email: 'igor@email.com',
-      senha: '1234',
-      perfis: ['0'],
-      dataCriacao: '13/09/2023'
-    }
-  ];
-
-  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  displayedColumns: string[] = ["id", "nome", "cpf", "email", "acoes"];
   dataSource = new MatTableDataSource<Tecnico>(this.TECNICO_DATA);
-  
-  ngOnInit(): void {
-    
-  }
-
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private service: TecnicoService) {}
 
-  ngAfterViewInit() {
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+      this.TECNICO_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Tecnico>(resposta);
+    });
     this.dataSource.paginator = this.paginator;
   }
-}
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
